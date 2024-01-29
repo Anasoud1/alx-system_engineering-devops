30d350f4c41a180dbcb180e1181bac2a94f31cda
Anasoud1 <anasoudsassi@gmail.com>
2024-01-29 18:23:20 +0100

    updating

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python3
 """
-Script that, using this REST API, for a given employee ID, returns
-information about his/her TODO list progress.
+A sript that, uses a REST API, for a given employee ID, returns
+information about his/her TODO list progress
+and exports data in the JSON format.
 """
-import requests
+
 import json
-import sys
+import requests
+from sys import argv
 
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com"
-    todo_url = url + '/users/{}/todos'.format(employee_id)
-    user_url = url + '/users/{}'.format(employee_id)
 
-    todo_response = requests.Session().get(todo_url)
-    user_response = requests.Session().get(user_url)
+    sessionReq = requests.Session()
 
-    todo_json = todo_response.json()
-    user = user_response.json().get('username')
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
 
-    list_dic = []
-    dic = {}
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
 
-    with open(f"{employee_id}.json", "w") as file:
-        for todo in todo_json:
-            list_dic.append(
-                    {
-                        'task': todo.get('title'),
-                        'completed': todo.get('completed'),
-                        'username': user,
-                    })
-        dic[employee_id] = list_dic
-        json.dump(dic, file)
+    json_req = employee.json()
+    usr = employeeName.json()['username']
+
+    totalTasks = []
+    updateUser = {}
+
+    for all_Emp in json_req:
+        totalTasks.append(
+            {
+                "task": all_Emp.get('title'),
+                "completed": all_Emp.get('completed'),
+                "username": usr,
+            })
+    updateUser[idEmp] = totalTasks
+
+    file_Json = idEmp + ".json"
+    with open(file_Json, 'w') as f:
+        json.dump(updateUser, f)
