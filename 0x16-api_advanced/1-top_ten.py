b03ae33ee7867e640aa14620432626e4fe556c28
Anasoud1 <anasoudsassi@gmail.com>
2024-02-13 14:50:07 +0100

    adding 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts listed"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'API Project'}
+    response = requests.get(url, headers=headers)
+    try:
+        datas = response.json().get('data').get('children')
+        for data in datas:
+            print(data.get('data').get('title'))
+    except Exception:
+        print("None")
