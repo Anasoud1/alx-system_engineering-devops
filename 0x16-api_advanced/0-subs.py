c5f7fb5e969bec8c65058df7750102978c0d6e89
Anasoud1 <anasoudsassi@gmail.com>
2024-02-13 17:45:22 +0100

    updating 0-subs.py and 1-top_ten.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
         return (0)
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'API Project'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     try:
         data = response.json()
         return data.get('data').get('subscribers')
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
     """prints the titles of the first 10 hot posts listed"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'API Project'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     try:
         datas = response.json().get('data').get('children')
         for data in datas:
